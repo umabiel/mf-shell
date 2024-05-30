28b152eaf6c2e486badcc3e672e7fc82abae234c
Gustavo I. Chalco <gchalcoa@gmail.com>
2024-05-30 12:25:17 -0500

    First commit mf-shell

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products',
+    // loadChildren: () =>
+    //   loadRemoteModule({
+    //     type: 'module',
+    //     remoteEntry: 'http://localhost:4201/remoteEntry.js',
+    //     exposedModule: 'ProductsModule',
+    //   }).then((m) => m.ProductsModule),
+
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: 'AppModule',
+      }).then((m) => m.AppModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
